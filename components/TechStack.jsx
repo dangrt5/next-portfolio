4604a807b82aa9c6a983c82556522aa51308f2d8
dangrt5 <randy.dang05@gmail.com>
2019-02-25 23:11:04 -0800

    Zeit now serverless deployment

--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 const TechStack = () => (
-    <div>
-        <h4 className="section-title">Skills</h4>
-        <h6 className="sub-section">Front End: JavaScript (ES6+), React, Redux, HTML, CSS (Flexbox), Bootstrap, jQuery</h6>
-        <h6>Back End: NodeJS, Express, MySQL</h6>
-        <h6>Tools: AWS, Github, Meistertask, Figma, Chrome Dev Tools</h6>
+    <div className="row">
+        <div className="col s12">
+            <h4 className="section-title">Skills</h4>
+            <h6 className="sub-section">Front End: JavaScript (ES6+), React, Redux, HTML, CSS (Flexbox), Bootstrap, jQuery</h6>
+            <h6>Back End: NodeJS, Express, MySQL</h6>
+            <h6>Tools: AWS, Github, Meistertask, Figma, Chrome Dev Tools</h6>
+        </div>
+
 
     </div>
 )
